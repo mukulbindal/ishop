55e8756a036cfbcafa96b18e151947586838760c
mukulbindal <mukulbindal170299@gmail.com>
2023-07-15 14:12:16 +0530

    Added sales

--- server.py
+++ server.py
@@ -16,4 +16,4 @@ app.include_router(user_router)
 app.include_router(product_router)
 app.include_router(sales_router)
 if __name__ == '__main__':
-    uvicorn.run('server:app', port=8000, reload=True)
+    uvicorn.run('server:app', port=8000)
